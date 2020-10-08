518600bdaacf1a4bf58e40629b322616fdacab3e
kotstot6 <72541310+kotstot6@users.noreply.github.com>
2020-10-07 23:47:58 -0700

    first version

---
+++ AddingMachine.java
@@ -0,0 +1,69 @@
+
+/**
+* The AddingMachine program performs addition and subtraction operations
+* and prints the results.
+*
+* @author  Kyle Otstot
+* @version 1.0
+* @since   2020-10-07
+*/
+
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+
+  /**
+   * This is the constuctor method of AddingMachine. Included for clarity.
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+
+  /**
+   * This accessor method returns the 'total' attribute
+   * @return int This returns the 'total' attribute.
+   */
+  public int getTotal () {
+    return 0;
+  }
+
+  /**
+   * This method adds the input to the total
+   * @param value This is added to 'total'
+   * @return Nothing.
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * This method subtracts the input from the total
+   * @param value This is subtracted from 'total'
+   * @return Nothing.
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * This method prints the history of transactions
+   * @return String This returns the transactions performed.
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * This method clears the operation and resets 'total' to 0.
+   */
+  public void clear() {
+  }
+
+  /**
+   * This is the main method which makes use of AddingMachine.
+   * @param args Unused.
+   * @return Nothing.
+   */
+  public static void main(String[] args)
+  {
+  }
+}
